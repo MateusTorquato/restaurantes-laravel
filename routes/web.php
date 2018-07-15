1c3383b089f7232365af097202381ee694301ddb
Mateus Torquato de Medeiros <matteus.torquato@gmail.com>
2018-07-15 00:34:29 -0300

    Fazendo testes de Insert, Update e Delete

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,50 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test/show', function () {
+    // $sql = 'SELECT * FROM USERS WHERE ID = ?';
+    // $users = DB::select($sql, [4]);
+
+    // $users = DB::table('users')
+    //              ->where('id', '=', 4)
+    //              ->select('id', 'name')
+    //              ->first();
+    //              // ->toSql();
+    //              // ->get();
+
+    // $users = \App\User::all();
+    $users = \App\User::where('id', 4)->get();
+
+    dd($users);
+});
+
+Route::get('/test/save', function () {
+  // $user = new \App\User();
+  // $user = \App\User::find(10);
+  //
+  // $user->name = 'Mateus';
+  // $user->email = 'teste@teste.com';
+  // $user->password = bcrypt('teste');
+  // $user->save();
+  $userData = [
+    'name' => 'Mateus',
+    'email' => 'testee',
+    'password' =>  bcrypt('teste')
+  ];
+   $user = new \App\User();
+   $user->create($userData);
+   // $user->update($userData);
+
+});
+
+Route::get('/test/delete', function () {
+  // $user = \App\User::find(10);
+  // $user->delete();
+  $user = \App\User::whereIn('id', [2,3,4]);
+  $user->delete();
+
+});
+
 Route::get('hello/{name}', function($name){
   // return view('hello',['name' => $name] );
   return view('hello', compact($name) );
