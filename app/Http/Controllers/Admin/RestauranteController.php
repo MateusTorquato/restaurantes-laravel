fcbb7a55d84c4339495471761000e07fc5524153
Mateus Torquato de Medeiros <matteus.torquato@gmail.com>
2018-08-04 20:05:03 -0300

    Iniciando cadastro do restaurante

---
+++ app/Http/Controllers/Admin/RestauranteController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RestauranteController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        return view('admin.restaurantes.store');
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/restaurantes/store.blade.php
@@ -0,0 +1,18 @@
+<h1>Inserção de Restaurantes</h1>
+<hr>
+
+<form action="">
+    <p>
+        <label>Nome do restaurante</label>
+        <input type="text" name="name">
+    </p>
+    <p>
+        <label>Endereço</label>
+        <input type="text" name="endereco">
+    </p>
+    <p>
+        <label>Fale sobre o restaurante</label>
+        <textarea name="descricao" id="" cols="30" rows="10"> </textarea>       
+    </p>
+    <input type="submit" value="Cadastrar">
+</form>
